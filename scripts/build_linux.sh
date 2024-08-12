8d24516a90117b4767d3f8a05ab3a2a467c759c7
Matthew Ballance <matt.ballance@gmail.com>
2024-08-12 01:09:30 -0700

    Include dependencies required for building ANTLR parser
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -x
 
+yum update -y
+yum install -y glibc-static
+yum install -y java-11-openjdk-devel uuid-devel libuuid-devel
+
 echo "BUILD_NUM=${BUILD_NUM}" >> python/zsp_sv/__build_num__.py
 ${IVPM_PYTHON} -m pip install ivpm cython
 ${IVPM_PYTHON} -m ivpm update -a
