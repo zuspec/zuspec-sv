db7903b6fb611dce4ecef17b1741999fd8195b83
Matthew Ballance <matt.ballance@gmail.com>
2024-09-06 00:33:54 +0000

    Add pkginfo
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

---
+++ python/zsp_sv/pkginfo.py
@@ -0,0 +1,23 @@
+import os
+import ivpm
+
+class PkgInfo(ivpm.PkgInfo):
+
+    def __init__(self):
+        pkgdir = os.path.dirname(os.path.abspath(__file__))
+        projdir = os.path.dirname(os.path.dirname(pkgdir))
+        super().__init__("zuspec-sv", os.path.dirname(pkgdir))
+
+        if os.path.isdir(os.path.join(projdir, "src")):
+            self._incdirs = [
+                os.path.join(projdir, "src", "include"),
+                os.path.join(projdir, "python")]
+            self._libdirs = [
+                os.path.join(projdir, "build", "lib"),
+                os.path.join(projdir, "build", "lib64")]
+        else:
+            self._incdirs = [os.path.join(pkgdir, "share", "include")]
+            self._libdirs = [os.path.join(pkgdir)]
+
+        self._deps = ["vsc-dm"]
+        self._libs = ["zsp-sv"]
