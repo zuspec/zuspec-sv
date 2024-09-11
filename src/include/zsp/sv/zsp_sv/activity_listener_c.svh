b471a9b789401828586364b875ac07f91705586f
Matthew Ballance <matt.ballance@gmail.com>
2024-09-11 13:58:23 +0000

    XX
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/include/zsp/sv/zsp_sv/activity_listener_c.svh
+++ src/include/zsp/sv/zsp_sv/activity_listener_c.svh
@@ -4,12 +4,12 @@ typedef class actor_c;
 class activity_listener_c;
 
     virtual function void enter_actor(
-        actor_c         actor,
+        actor_c         actor
     );
     endfunction
 
     virtual function void leave_actor(
-        actor_c         actor,
+        actor_c         actor
     );
     endfunction
 
