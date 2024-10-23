51948b4e4890620ec86111440ebbce547f0d6e71
Matthew Ballance <matt.ballance@gmail.com>
2024-10-23 02:42:58 +0000

    XX
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,4 +2,4 @@
 
 proj_dir=$(dirname $(dirname $(realpath $0)))
 
-python3 -m pytest ${proj_dir}/tests/test_exec_stmts.py
+./packages/python/bin/python3 -m pytest ${proj_dir}/tests/test_exec_stmts.py
