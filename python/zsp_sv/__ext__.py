a028e0eecc4601b9969d1e0d86589a4b84c31bd8
Matthew Ballance <matt.ballance@gmail.com>
2024-10-29 13:51:40 +0000

    Add command to get 'share' directory
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- python/zsp_sv/__ext__.py
+++ python/zsp_sv/__ext__.py
@@ -1,5 +1,7 @@
 from zuspec.cli import add_subcommand
 from .gen.cmd_generate_sv import CmdGenerateSv
 from .gen.cmd_gen_sv_actor import CmdGenSvActor
+from .gen.cmd_gen_sv_share import CmdGenSvShare
 
 add_subcommand("synth.sv.actor", CmdGenSvActor.register)
+add_subcommand("synth.sv.share", CmdGenSvShare.register)
