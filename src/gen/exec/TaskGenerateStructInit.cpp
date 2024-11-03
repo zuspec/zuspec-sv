7f836e5371b9ddebf1e2eeb081a514f5be3f4294
Matthew Ballance <matt.ballance@gmail.com>
2024-11-03 16:43:29 +0000

    XX
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/gen/exec/TaskGenerateStructInit.cpp
+++ src/gen/exec/TaskGenerateStructInit.cpp
@@ -60,13 +60,11 @@ void TaskGenerateStructInit::generate_tail(vsc::dm::IDataTypeStruct *t) {
 }
 
 void TaskGenerateStructInit::visitDataTypeArray(vsc::dm::IDataTypeArray *t) {
-    DEBUG_ENTER("visitDataTypeArray");
     if (m_field->getInit()) {
         for (uint32_t i=0; i<t->getSize(); i++) {
 
         }
     }
-    DEBUG_LEAVE("visitDataTypeArray");
 }
 
 void TaskGenerateStructInit::visitDataTypeComponent(arl::dm::IDataTypeComponent *t) {
