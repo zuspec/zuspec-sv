59fdfd0f0452f203fb1fb7a5f86db8b8e5ee212f
Matthew Ballance <matt.ballance@gmail.com>
2024-11-03 01:50:31 +0000

    Correct compilation error
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/gen/exec/TaskGenerateExecScope.cpp
+++ src/gen/exec/TaskGenerateExecScope.cpp
@@ -208,9 +208,9 @@ void TaskGenerateExecScope::visitTypeProcStmtExpr(arl::dm::ITypeProcStmtExpr *s)
 
 void TaskGenerateExecScope::visitTypeProcStmtForeach(arl::dm::ITypeProcStmtForeach *s) {
     DEBUG_ENTER("visitTypeProcStmtForeach");
-    m_genref->pushScope(s);
+//    m_genref->pushScope(s);
 
-    m_genref->popScope();
+//    m_genref->popScope();
     DEBUG_LEAVE("visitTypeProcStmtForeach");
 }
 
