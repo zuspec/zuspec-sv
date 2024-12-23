48b37e2b110853a6e3e8367612240339f1f7def6
Matthew Ballance <matt.ballance@gmail.com>
2024-12-23 20:41:18 +0000

    Add copyright headers
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/include/zsp/sv/zsp_sv/activity_seq_c.svh
+++ src/include/zsp/sv/zsp_sv/activity_seq_c.svh
@@ -1,6 +1,24 @@
+/*
+ * activity_seq_c.svh
+ *
+ * Copyright 2023 Matthew Ballance and Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may 
+ * not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
+ * Created on:
+ *     Author:
+ */
 
-// SystemVerilog classes do not use braces. The end the class declaration with a semicolon.
-// SystemVerilog classes have virtual methods, but do not use '= 0' to indicate pure virtual methods.
 class activity_seq_c : public activity_c;
     activity_c      sub_activities[$];
 
