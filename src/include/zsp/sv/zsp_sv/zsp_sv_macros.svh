f6244ac2a62ea90e248b3a89979837d1d987dabf
Matthew Ballance <matt.ballance@gmail.com>
2025-01-28 02:11:44 +0000

    Correct sim-crash during register deref
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/include/zsp/sv/zsp_sv/zsp_sv_macros.svh
+++ src/include/zsp/sv/zsp_sv/zsp_sv_macros.svh
@@ -21,9 +21,9 @@
 `ifndef INCLUDED_ZSP_SV_MACROS_SVH
 `define INCLUDED_ZSP_SV_MACROS_SVH
 
-`define zsp_reg_type_offset(regtype, regname) regtype``::``inst(null).``regname``.``offset
+`define zsp_reg_type_offset(regtype, regname) regtype``::``prv_inst.``regname``.``offset
 
-`define zsp_reg_type_arr_offset(regtype, regname, index) regtype``::``inst(null).``regname``.``offsets[``index``]
+`define zsp_reg_type_arr_offset(regtype, regname, index) regtype``::``prv_inst.``regname``.``offsets[``index``]
 
 `define zsp_inc(obj) if (obj != null) obj .inc()
 
