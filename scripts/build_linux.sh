19aa5bff1bc07457c1ef72c99175e1fc8c00ba4f
Matthew Ballance <matt.ballance@gmail.com>
2025-02-10 04:52:41 +0000

    Ensure setuptools is installed
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -5,7 +5,7 @@ yum install -y glibc-static
 yum install -y java-11-openjdk-devel uuid-devel libuuid-devel
 
 echo "BUILD_NUM=${BUILD_NUM}" >> python/zsp_sv/__build_num__.py
-${IVPM_PYTHON} -m pip install ivpm cython
+${IVPM_PYTHON} -m pip install --pre ivpm cython setuptools
 ${IVPM_PYTHON} -m ivpm update -a --py-prerls-packages
 
 echo "Requirements Files"
